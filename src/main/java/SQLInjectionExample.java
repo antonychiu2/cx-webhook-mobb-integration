70255e5760ca1de71bd2ff9b3a0db9b2b7a8a231
Antony Chiu <antony@mobb.ai>
2024-11-21 12:34:19 -0700

    Update SQLInjectionExample.java

--- src/main/java/SQLInjectionExample.java
+++ src/main/java/SQLInjectionExample.java
@@ -13,11 +13,11 @@ public class SQLInjectionExample extends HttpServlet {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db");
 
             String query = "SELECT * FROM users WHERE user name = '" + request.getParameter("username") + "';";
-            Statement stmt = con.createStatement();
+            Statement stmt = con.createStatement(); 
 
             stmt.executeQuery(query);
-        } catch (Exception e) {
-            throw new ServletException(e);
+        } catch (Exception e) { 
+            throw new ServletException(e); 
         }
     }
 }
